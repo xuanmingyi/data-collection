5d17aba5a31d76a0cc9e1163dd5f1799d7816224
sin <xuanmingyi@gmail.com>
2021-06-30 05:24:18 +0800

    third_party

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+for i in `find api -name *.proto|xargs`;do
+    protoc --proto_path=. --proto_path=third_party --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. $i
+    protoc --proto_path=. --proto_path=third_party --go_out=paths=source_relative:. --go-http_out=paths=source_relative:. $i
+done
+
 cd app/file
 
 for i in `find . -name *.proto|xargs`;do
     protoc --proto_path=. --go_out=paths=source_relative:. --go-http_out=paths=source_relative:. $i
 done
+
 
 cd internal/data
 ent generate ./ent/schema
